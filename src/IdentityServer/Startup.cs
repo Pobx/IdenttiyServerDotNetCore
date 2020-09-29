add43d2e6afc226683779fa4d724684c5cd93117
Pobx <geidtiphong.s@allrightbox.com>
2020-09-29 14:44:01 +0700

    comment InitializeDatabase after create database schema

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -48,6 +48,7 @@ namespace IdentityServer {
     }
 
     public void Configure (IApplicationBuilder app) {
+      // InitializeDatabase (app);
       if (Environment.IsDevelopment ()) {
         app.UseDeveloperExceptionPage ();
       }
@@ -65,41 +66,33 @@ namespace IdentityServer {
       });
     }
 
-    private void InitializeDatabase(IApplicationBuilder app)
-{
-    using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
-    {
-        serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+    private void InitializeDatabase (IApplicationBuilder app) {
+      using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory> ().CreateScope ()) {
+        serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext> ().Database.Migrate ();
 
-        var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
-        context.Database.Migrate();
-        if (!context.Clients.Any())
-        {
-            foreach (var client in Config.Clients)
-            {
-                context.Clients.Add(client.ToEntity());
-            }
-            context.SaveChanges();
+        var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext> ();
+        context.Database.Migrate ();
+        if (!context.Clients.Any ()) {
+          foreach (var client in Config.Clients) {
+            context.Clients.Add (client.ToEntity ());
+          }
+          context.SaveChanges ();
         }
 
-        if (!context.IdentityResources.Any())
-        {
-            foreach (var resource in Config.IdentityResources)
-            {
-                context.IdentityResources.Add(resource.ToEntity());
-            }
-            context.SaveChanges();
+        if (!context.IdentityResources.Any ()) {
+          foreach (var resource in Config.IdentityResources) {
+            context.IdentityResources.Add (resource.ToEntity ());
+          }
+          context.SaveChanges ();
         }
 
-        if (!context.ApiScopes.Any())
-        {
-            foreach (var resource in Config.ApiScopes)
-            {
-                context.ApiScopes.Add(resource.ToEntity());
-            }
-            context.SaveChanges();
+        if (!context.ApiScopes.Any ()) {
+          foreach (var resource in Config.ApiScopes) {
+            context.ApiScopes.Add (resource.ToEntity ());
+          }
+          context.SaveChanges ();
         }
+      }
     }
-}
   }
 }
